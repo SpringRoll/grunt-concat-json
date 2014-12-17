bbca56a9b399218556b4e74763099b913d405531
Matte Szklarz <matteszklarz@cloudkid.com>
2014-12-17 17:33:01 -0500

    formatting

--- tasks/concat-json.js
+++ tasks/concat-json.js
@@ -1,26 +1,24 @@
 module.exports = function(grunt)
 {
-var chalk = require("chalk");
-var stripJsonComments = require("strip-json-comments");
-var jsonlint = require("jsonlint");
+  var chalk = require("chalk");
+  var stripJsonComments = require("strip-json-comments");
+  var jsonlint = require("jsonlint");
 
-grunt.registerMultiTask("concat-json", "Merge Multiple JSON Files", function()
-{
-// prepare options
-var options = this.options(
-{
-    replacer: null,
-    space: "\t",
-    folderArrayMarker : '[]'
-});
+  grunt.registerMultiTask("concat-json", "Merge Multiple JSON Files", function()
+  {
+    // prepare options
+    var options = this.options({
+      replacer: null,
+      space: "\t",
+      folderArrayMarker: '[]'
+    });
 
-grunt.verbose.writeflags(options, "Options");
+    grunt.verbose.writeflags(options, "Options");
 
-// Iterate over all src-dest file pairs
-this.files.forEach(function(f)
-{
-    try
+    // Iterate over all src-dest file pairs
+    this.files.forEach(function(f)
     {
+      try {
         // Start with an empty object
         var json = {};
 
@@ -31,148 +29,147 @@ this.files.forEach(function(f)
         f.src.forEach(function(src)
         {
 
-            // Merge JSON file into object
-            if (!grunt.file.exists(src))
-            {
-                throw "JSON source file \"" + chalk.red(src) + "\" not found.";
-            }
-            else
-            {
-                var fragment;
+          // Merge JSON file into object
+          if (!grunt.file.exists(src))
+          {
+            throw "JSON source file \"" + chalk.red(src) + "\" not found.";
+          }
+          else
+          {
+            var fragment;
 
-                try
+            try {
+              // Read the raw file
+              var withComments = grunt.file.read(src);
+
+              // Strip the file of the comments
+              var without = stripJsonComments(withComments);
+
+              // Lint the comment-free file.
+              // If linting errors, terminal will let you know!
+              var linted = jsonlint.parse(without);
+
+              fragment = {
+                dir: '',
+                // Attach comment-less JSON
+                json: linted
+              };
+
+              // Start a top level
+              var currentDir = json;
+
+              // Remove the path to the contianer,
+              // and the .json extension
+              var path = src.replace(f.base + '/', '').replace('.json', '');
+
+              var test = true;
+              while (test)
+              {
+                test = testDirectory(path, currentDir);
+              }
+
+              /**
+               *
+               *
+               * @param {String}
+               * @param {String}
+               */
+              function testDirectory(_path, _currentDir)
+              {
+                var _currentDirIsArray = Array.isArray(_currentDir);
+
+                // If the is a slash, we have a parent folder
+                var firstSlash = _path.indexOf('/');
+                if (firstSlash > -1)
                 {
-                    // Read the raw file
-                    var withComments = grunt.file.read(src);
+                  var dir = _path.slice(0, firstSlash);
+                  if (grunt.util._.has(_currentDir, dir) === false)
+                  {
+                    _currentDir[dir] = {};
+                  }
 
-                    // Strip the file of the comments
-                    var without = stripJsonComments(withComments);
-
-                    // Lint the comment-free file.
-                    // If linting errors, terminal will let you know!
-                    var linted = jsonlint.parse(without);
-
-                    fragment = {
-                        dir: '',
-                        // Attach comment-less JSON
-                        json: linted
-                    };
-
-                    // Start a top level
-                    var currentDir = json;
-
-                    // Remove the path to the contianer,
-                    // and the .json extension
-                    var path = src.replace(f.base + '/', '').replace('.json', '');
-
-                    var test = true;
-                    while (test)
-                    {
-                        test = testDirectory(path, currentDir);
-                    }
-
-/**
-*
-*
-* @param {String}
-* @param {String}
-*/
-                    function testDirectory(_path, _currentDir)
-                    {
-                        var _currentDirIsArray = Array.isArray(_currentDir);
-
-                        // If the is a slash, we have a parent folder
-                        var firstSlash = _path.indexOf('/');
-                        if (firstSlash > -1)
-                        {
-                            var dir = _path.slice(0, firstSlash);
-                            if (grunt.util._.has(_currentDir, dir) === false)
-                            {
-                                _currentDir[dir] = {};
-                            }
-
-                            currentDir = _currentDir[dir];
-                            path = _path.slice(firstSlash+1);
-                            return true;
-                        }
-
-                        _currentDir[path] =  fragment.json;
-                        return false;
-                    }
+                  currentDir = _currentDir[dir];
+                  path = _path.slice(firstSlash + 1);
+                  return true;
                 }
-                catch (e)
-                {
-                    grunt.fail.warn(e);
-                }
+
+                _currentDir[path] = fragment.json;
+                return false;
+              }
             }
+            catch (e)
+            {
+              grunt.fail.warn(e);
+            }
+          }
         });
 
         /**
-        * Search the JSON object for the folders (now object keys) that
-        * were marked to be arrays, convert the values to array items.
-        * Process removes the folderArrayMarker from the final JSON file,
-        * so you can access keys in code without the special symbol.
-        * i.e. if folderArrayMarker is the default '[]' then
-        * the "directory[]": key becomes "directory"
-        *
-        * Reminder, if an folder-array is nested in a folder-array, only
-        * the top level folder-array will get a name change, as the children
-        * arrays will become nameless array index items.
-        *
+         * Search the JSON object for the folders (now object keys) that
+         * were marked to be arrays, convert the values to array items.
+         * Process removes the folderArrayMarker from the final JSON file,
+         * so you can access keys in code without the special symbol.
+         * i.e. if folderArrayMarker is the default '[]' then
+         * the "directory[]": key becomes "directory"
+         *
+         * Reminder, if an folder-array is nested in a folder-array, only
+         * the top level folder-array will get a name change, as the children
+         * arrays will become nameless array index items.
+         *
          * @param {Object} _obj
          */
         var convertArrayMarkedFolders = function(_obj)
         {
-            for (var key in _obj)
+          for (var key in _obj)
+          {
+            // Check all keys for the folderArrayMarker
+            var indexOfMarker = key.indexOf(options.folderArrayMarker);
+            var folderIsArray = indexOfMarker > -1;
+            if (folderIsArray)
             {
-                // Check all keys for the folderArrayMarker
-                var indexOfMarker = key.indexOf(options.folderArrayMarker);
-                var folderIsArray = indexOfMarker > -1;
-                if (folderIsArray)
-                {
-                    /* Send contents through recursively before doing
-                       the contents copy.
-                       The process removes all but the top level key. */
-                    convertArrayMarkedFolders(_obj[key]);
-                    var contents = _obj[key];
+              /* Send contents through recursively before doing
+                 the contents copy.
+                 The process removes all but the top level key. */
+              convertArrayMarkedFolders(_obj[key]);
+              var contents = _obj[key];
 
-                    /* Push the values one by one of the original key
-                       into a new fresh array who's key has the
-                       folderArrayMarker removed from it */
-                    var keyWithoutMarker = key.slice(0, indexOfMarker);
-                    _obj[keyWithoutMarker] = [];
-                    for (var k in contents)
-                    {
-                       _obj[keyWithoutMarker].push(contents[k]);
-                    };
+              /* Push the values one by one of the original key
+                 into a new fresh array who's key has the
+                 folderArrayMarker removed from it */
+              var keyWithoutMarker = key.slice(0, indexOfMarker);
+              _obj[keyWithoutMarker] = [];
+              for (var k in contents)
+              {
+                _obj[keyWithoutMarker].push(contents[k]);
+              }
 
-                    // Delete the original marker key.
-                   delete _obj[key];
-                }
-
+              // Delete the original marker key.
+              delete _obj[key];
             }
+
+          }
         };
 
         convertArrayMarkedFolders(json);
 
         // Write object as new JSON
         grunt.log.debug(
-            "writing JSON destination file \"" +
-            chalk.green(f.dest) +
-            "\"");
+          "writing JSON destination file \"" +
+          chalk.green(f.dest) +
+          "\"");
 
         grunt.file.write(
-            f.dest,
-            JSON.stringify(json, options.replacer, options.space));
+          f.dest,
+          JSON.stringify(json, options.replacer, options.space));
 
         grunt.log.writeln("File \"" +
-            chalk.green(f.dest) +
-            "\" created.");
-    }
-    catch (e)
-    {
+          chalk.green(f.dest) +
+          "\" created.");
+      }
+      catch (e)
+      {
         grunt.fail.warn(e);
-    }
-});
-});
+      }
+    });
+  });
 };
